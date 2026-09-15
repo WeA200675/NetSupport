e254231d09f87d0e485845b34ca48397f95e85c9
WeA200675 <78141602+WeA200675@users.noreply.github.com>
2026-09-15 05:38:54 +0200

    Remove RDP session launcher contract

--- src/NetSupport.RemoteAdmin/Services/IRdpSessionLauncher.cs
+++
@@ -1,9 +0,0 @@
-using NetSupport.RemoteAdmin.Models;
-
-namespace NetSupport.RemoteAdmin.Services;
-
-public interface IRdpSessionLauncher
-{
-    bool IsAvailable { get; }
-    Task LaunchAsync(RemoteTarget target, CancellationToken cancellationToken = default);
-}
