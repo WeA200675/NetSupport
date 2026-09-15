1061ec9ad63adee57e1b998d3ff77bb1bd86a822
WeA200675 <78141602+WeA200675@users.noreply.github.com>
2026-09-15 05:39:23 +0200

    Remove legacy RDP application settings

--- src/NetSupport.RemoteAdmin/Services/AppConfig.cs
+++ src/NetSupport.RemoteAdmin/Services/AppConfig.cs
@@ -6,8 +6,6 @@ public sealed class AppConfig
 {
     public string? NetSupportExecutable { get; set; }
     public bool StartMinimized { get; set; }
-    public bool UseFullScreenRdp { get; set; }
-    public bool UseEmbeddedRdp { get; set; } = true;
     public bool DiagnosticLoggingEnabled { get; set; } = true;
     public List<RemoteTarget> Targets { get; set; } = [];
     public List<SavedTargetView> SavedViews { get; set; } = [];
